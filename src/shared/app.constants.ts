49028dffa3538992a478cb45f37ae80d8a5f81d2
Léon Mangeat <leon.mangeat@gmail.com>
2017-04-14 15:15:41 +0200

    API consuming URL correction

--- src/shared/app.constants.ts
+++ src/shared/app.constants.ts
@@ -5,7 +5,7 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class Configuration {
-  public Server = 'http://localhost:10010';
+  public Server = 'http://localhost:10010/';
   public ApiUrl = 'api/';
   public ServerWithApiUrl = this.Server + this.ApiUrl;
 }
