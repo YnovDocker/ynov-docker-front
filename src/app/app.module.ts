b40ac943919e12e3a480ce8bba99bd3457450a1e
Thomas Doret <thomas.doret33@gmail.com>
2017-04-28 13:26:27 +0200

    finish upload & manage image, TODO viewer and reach the image folder from the server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,10 @@ import {LogoComponent} from './shared/logo/logo.component';
 import {MenuComponent} from './shared/menu/menu.component';
 import {AuthComponent} from './auth/auth.component';
 import {RegisterComponent} from './register/register.component';
-import {TestUploadComponent} from './test-upload/test-upload.component';
+import {TestUploadComponent} from './manage/test-upload/test-upload.component';
 import { LogoutComponent } from './logout/logout.component';
+import { ViewerComponent } from './viewer/viewer.component';
+import { PanelComponent } from './manage/panel/panel.component';
 
 
 @NgModule({
@@ -27,7 +29,9 @@ import { LogoutComponent } from './logout/logout.component';
     AuthComponent,
     RegisterComponent,
     TestUploadComponent,
-    LogoutComponent
+    LogoutComponent,
+    ViewerComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule,
@@ -54,12 +58,16 @@ import { LogoutComponent } from './logout/logout.component';
         component: RegisterComponent
       },
       {
-        path: 'upload',
-        component: TestUploadComponent
-      },
-      {
         path: 'user',
         component: UserComponent
+      },
+      {
+        path: 'manage',
+        component: PanelComponent
+      },
+      {
+        path: 'view',
+        component: ViewerComponent
       },
       {
         path: 'logout',
